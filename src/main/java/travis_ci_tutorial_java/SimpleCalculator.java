20fa0fc5245403a2786866f78440ae8d743aee35
Tyreik <tyreitix@gmail.com>
2021-12-12 19:48:44 -0500

    Added minus, multiply and divide methods

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,23 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	
+	public int divide(int i, int j) {
+		// TODO Auto-generated method stub
+		if(j != 0)
+			return i/j;
+		else
+			System.out.println("Cannot divide by 0");
+			return 0;
+	}
+	
+	public int minus(int i, int j) {
+		// TODO Auto-generated method stub
+		return i-j;
+	}
+	
+	public int multiply(int i, int j) {
+		// TODO Auto-generated method stub
+		return i*j;
+	}
 }
